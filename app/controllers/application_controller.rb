c4336592a5f9ded262a73f14137e25622134f879
Tim Green <tiimgreen@gmail.com>
2015-02-23 23:31:57 +0000

    Update views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,24 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :set_title
+  before_action :set_title, :dashboard_redirect
 
-  def set_title
-    case params[:action]
-    when 'index'
-      @page_title = params[:controller].titleize
-    when 'show'
-      @page_title = params[:controller].singularize.titleize
-    when 'new'
-      @page_title = "New #{params[:controller].singularize.titleize}"
-    else
-      @page_title = params[:action].titleize
+  private
+
+    def set_title
+      case params[:action]
+      when 'index'
+        @page_title = params[:controller].titleize
+      when 'show'
+        @page_title = params[:controller].singularize.titleize
+      when 'new'
+        @page_title = "New #{params[:controller].singularize.titleize}"
+      else
+        @page_title = params[:action].titleize
+      end
     end
-  end
+
+    def dashboard_redirect
+      redirect_to dashboard_path if user_signed_in?
+    end
 end

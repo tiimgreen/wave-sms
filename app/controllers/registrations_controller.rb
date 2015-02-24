5cf2f77e51fb4a493b98927840a97fbd00572fd3
Tim Green <tiimgreen@gmail.com>
2015-02-24 00:13:51 +0000

    Add Organisation -> User relation, devise views

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,30 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  def create
+    redirect_to root_path
+
+    @org = Organisation.create!(name: params[:organisation_name])
+
+    params.delete(:organisation_name)
+
+    build_resource(sign_up_params.merge({ organisation_id: @org.id }))
+
+    resource_saved = resource.save
+    yield resource if block_given?
+    if resource_saved
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_flashing_format?
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      @org.destroy
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
+end

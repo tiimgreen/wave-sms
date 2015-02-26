f643ea47f38b3d29fb1c320077da50ca4669ba68
Tim Green <tiimgreen@gmail.com>
2015-02-26 21:39:10 +0000

    Refactor Organisations controller

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -26,24 +26,20 @@ class OrganisationsController < ApplicationController
   def choose_phone_number
     @org = Organisation.find(params[:organisation_id])
 
-    @client = Twilio::REST::Client.new(ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"])
-
     if params[:area_code] == 'none'
-      @numbers = @client.available_phone_numbers.get('GB').local.list
+      @numbers = client.available_phone_numbers.get('GB').local.list
     else
-      @area_code = twilio_area_code(current_user.organisation.area_code)
-
-      @numbers = @client.available_phone_numbers.get('GB').local.list(contains: @area_code)
+      area_code = twilio_area_code(current_user.organisation.area_code)
+      @numbers = client.available_phone_numbers.get('GB').local.list(contains: area_code)
     end
   end
 
   def activate_phone_number
-    @org = Organisation.find(params[:organisation_id])
+    org = Organisation.find(params[:organisation_id])
 
-    @client = Twilio::REST::Client.new(ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"])
-    @client.incoming_phone_numbers.create(phone_number: params[:phone_number])
+    client.incoming_phone_numbers.create(phone_number: params[:phone_number])
 
-    if @org.update_attributes(phone_number: params[:phone_number])
+    if org.update_attributes(phone_number: params[:phone_number])
       flash[:success] = 'Phone number activated!'
       redirect_to dashboard_path
     end
@@ -52,18 +48,18 @@ class OrganisationsController < ApplicationController
   private
 
     def authenticate_org_owner
-      @org = Organisation.find(params[:id])
+      org = Organisation.find(params[:id])
 
-      unless current_user == @org.owner
+      unless current_user == org.owner
         flash[:warning] = 'You need to be the organisation owner to do that.'
         redirect_to dashboard_path
       end
     end
 
     def authenticate_has_phone_number
-      @org = Organisation.find(params[:organisation_id])
+      org = Organisation.find(params[:organisation_id])
 
-      if @org.phone_number.present?
+      if org.phone_number.present?
         flash[:warning] = 'You already have a phone number.'
         redirect_to dashboard_path
       end
@@ -71,5 +67,9 @@ class OrganisationsController < ApplicationController
 
     def organisation_params
       params.require(:organisation).permit(:name, :area_code)
+    end
+
+    def client
+      Twilio::REST::Client.new(ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"])
     end
 end

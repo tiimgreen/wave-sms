b69cfb775ff954cb813305b3a33dd6365f6aed87
Tim Green <tiimgreen@gmail.com>
2015-03-10 02:21:43 +0000

    Update views

--- app/controllers/staff_controller.rb
+++ app/controllers/staff_controller.rb
@@ -1,6 +1,6 @@
 class StaffController < ApplicationController
   before_action :authenticate_user!
-  before_action :over_limit, only: %i( new create )
+  before_action :over_staff_limit, only: %i( new create )
 
   include ActionView::Helpers::UrlHelper
 
@@ -16,7 +16,7 @@ class StaffController < ApplicationController
 
   private
 
-    def over_limit
+    def over_staff_limit
       if current_org.staff.count >= current_org.staff_limit
         flash[:warning] = "You have reached your staff limit of #{current_org.staff_limit}. Need to #{link_to('upgrade your account?', upgrade_organisation_path, class: 'alert-link')}"
         redirect_to staff_index_path
